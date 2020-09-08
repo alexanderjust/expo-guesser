f493287c925057bb2c5a84fa41aa0b470985db99
Alex Bolhovitin <Alex.Bolhovitin@ptw.com>
2020-09-08 11:26:16 +0100

    updated GameOverScreen with BodyText components

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
-import Card from '../components/Card'
+import Card from '../components/Card';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+
 
 function GameOverScreen(props) {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Number of Rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <TitleText>The Game is Over!</TitleText>
+      <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
+      <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title='NEW GAME' onPress={props.onRestart} />
     </View>
   );
